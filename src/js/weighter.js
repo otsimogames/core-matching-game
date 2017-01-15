4c4bcda18b99de69d0cfe693a20f50702d1b0807
Sercan Degirmenci <sercan@otsimo.com>
2017-01-16 02:23:04 +0300

    add labels field to manifest

--- src/js/weighter.js
+++ src/js/weighter.js
@@ -89,7 +89,9 @@ export class RemoteProvider extends AbstractWeightProvider {
     }
     req.open('POST', this.url);
     req.setRequestHeader('Content-Type', 'application/json');
-    req.send(JSON.stringify({ childId: this.child, gameId: this.game, interval: interval || 0 }));
+    const reqData = JSON.stringify({ childId: this.child, gameId: this.game, interval: `${interval}` });
+    console.log('req data is', reqData);
+    req.send(reqData);
   }
 }
 
