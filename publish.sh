f4b8ebfde3d81508576e85b3fac41707460ad2cb
Ünal Akünal <unal@otsimo.com>
2017-09-12 14:45:32 +0300

    update publish.sh & send v1.9.1 to npm

--- publish.sh
+++ publish.sh
@@ -8,7 +8,7 @@ if [ "$TRAVIS_TAG" ];then
     mkdir -p builds/npm
     cp -r src/js/* builds/npm
     cp package.json builds/npm
-    cp settings.json builds/npm
+    cp src/settings.json builds/npm
 
     cd builds/npm
     npm publish
