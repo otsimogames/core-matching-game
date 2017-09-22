7b8341e7aaae5e3f7b2aa8948352f3dcbab8d59c
Ünal Akünal <unal@otsimo.com>
2017-09-22 18:32:33 +0300

    add load_bar to boot.js

--- src/js/states/boot.js
+++ src/js/states/boot.js
@@ -1,8 +1,10 @@
+import img from "../../assets/load_bar.png"
+
 export default class Boot extends Phaser.State {
     preload() {
         // If needed the image for loader bar in preload state can be loaded here
         if (otsimo.kv.loading_bar.sprite) {
-            this.game.load.image(otsimo.kv.loading_bar.sprite);
+            this.game.load.image("load_bar", img);
         }
     }
 
